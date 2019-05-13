9bd00a504585e4979543b0307eee911494b956a5
Leonardo Augusto <leonardo@codebit.com.br>
2019-05-13 11:59:29 -0300

    Replace multiple words too

--- parse_file.py
+++ parse_file.py
@@ -1,5 +1,8 @@
+import itertools
+import time
 
 
+# Read file and return an array of text
 def read_file(filename):
 	f = open(filename, 'r')
 	rows = f.readlines()
@@ -7,23 +10,38 @@ def read_file(filename):
 	return rows
 
 
-def parse_file():
-	filename = 'filename.txt'
-	new_file = 'new_file.txt'
+def replace(row, to_rep, rep_for):
+	row = row.replace(to_rep, rep_for)
+
+	return row
+
+
+"""	Parse file replacing your desire words
+	Write a new file and close it """
+def parse_file(filename, new_filename, to_replace, replace_for):
 	rows = read_file(filename)
-	new_file = open(new_file, 'w')
-	to_replace = 'Fizz'
-	replace_for = 'Buzz'
+	new_file = open(new_filename, 'w')
 
 	i = 1
+	start = time.time()
 	for row in rows:
-		if to_replace in row:
-			row = row.replace(to_replace, replace_for)
+		for to_rep, rep_for in zip(to_replace, replace_for):
+			if to_rep in row:
+				row = replace(row, to_rep, rep_for)
+				print('Modifying row: {}'.format(i))
 		new_file.write(row)
-		print('Modifying row: {}'.format(i))
 		i += 1
 
 	new_file.close()
+	finished = time.time() - start
+	print('Finished in {}'.format(finished))
 
 
-parse_file()
+
+filename = 'to_modify.txt'
+new_filename = 'modified.txt'
+to_replace = 'my', 'old', 'words'
+replace_for = 'your', 'new', 'text'
+
+
+parse_file(filename, new_filename, to_replace, replace_for)
